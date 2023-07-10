4a7b6e44a99918a0926a687ecd5a75f9df115ccf
Nikanov Paul <paha_feed@mail.ru>
2023-07-10 14:07:04 +0300

    Change some files

--- Quarqus/src/main/java/com/example/controller/MyController.java
+++ Quarqus/src/main/java/com/example/controller/MyController.java
@@ -1,19 +1,14 @@
 package com.example.controller;
 
-import com.example.entity.Buildings;
+import com.example.entity.Building;
 import com.example.repository.BuildingsRepository;
 
 import javax.inject.Inject;
-import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
-import javax.json.JsonReader;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -22,63 +17,45 @@ public class MyController {
     @Inject
     BuildingsRepository BuildingsRepository;
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    @Path("/update")
-    public void updateFrom() throws FileNotFoundException {
-        String filename = "/home/paul/source/practice2023/lpu.json";
-        InputStream fis = new FileInputStream(filename);
-        JsonReader reader = Json.createReader(fis);
-        JsonArray personObject = reader.readArray();
-        BuildingsRepository.addOrModify(personObject);
+    @POST
+//    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/updateJSONS")
+    public void updateFromJSONS(JsonArray jsonArray) {
+//        String filename = "/home/paul/source/practice2023/lpu.json";
+//        InputStream fis = new FileInputStream(filename);
+//        JsonReader reader = Json.createReader(fis);
+//        JsonArray personObject = reader.readArray();
+        BuildingsRepository.addOrModify(jsonArray);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/updateJSON")
+    public void updateFromJSON(JsonObject jsonObject) {
+        BuildingsRepository.addOrModify(jsonObject);
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/getFrom")
-    public List<Buildings> getFrom(@QueryParam("value") int value) {
-        return BuildingsRepository.listAll();
-    }
-
-//    @GET
-//    @Produces(MediaType.TEXT_PLAIN)
-//    public String hello() {
-//        logger.info("Hello");
-//        return "hello";
-//    }
-
-    @POST
-    @Path("/add/")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Transactional
-    public String add(Buildings obj) throws SQLException {
-        BuildingsRepository.addOrModify(obj);
-        return "Added " + obj.getName();
-    }
-
-    @POST
-    @Path("/addJson/")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Transactional
-    public void addJSON(JsonObject obj) throws SQLException {
-        BuildingsRepository.addOrModify(obj);
-
+    public List<Building> getFrom(@QueryParam("lat") double lat, @QueryParam("lon") double lon, @QueryParam("dist") double dist) {
+        return BuildingsRepository.getAllWith(lat, lon, dist);
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/getAll")
     @Transactional
-    public List<Buildings> getAll() {
-        return BuildingsRepository.findAll().list();
+    public List<Building> getAll() {
+        return BuildingsRepository.listAll();
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Transactional
     @Path("/get/{id}")
-    public Buildings getByID(@PathParam("id") long id) {
+    public Building getByID(@PathParam("id") long id) {
         return BuildingsRepository.findById(id);
     }
 
--- Quarqus/src/main/java/com/example/repository/BuildingsRepository.java
+++ Quarqus/src/main/java/com/example/repository/BuildingsRepository.java
@@ -1,32 +1,55 @@
 package com.example.repository;
 
-import com.example.entity.Buildings;
+import com.example.entity.Building;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static java.lang.Math.cos;
+
 @ApplicationScoped
-public class BuildingsRepository implements PanacheRepository<Buildings> {
+public class BuildingsRepository implements PanacheRepository<Building> {
 
     @Transactional
-    public void addOrModify(Buildings obj) {
-        Optional<Buildings> old = findByIdOptional(obj.getId());
+    public void addOrModify(Building obj) {
+        Optional<Building> old = findByIdOptional(obj.getId());
         if (old.isPresent() && (old.get().getLast_update() >= obj.getLast_update()))
             return;
         persist(obj);
     }
 
     public void addOrModify(JsonObject obj) {
-        addOrModify(new Buildings(obj));
+        addOrModify(new Building(obj));
     }
 
     public void addOrModify(JsonArray jsonArray) {
         for (javax.json.JsonValue value : jsonArray) {
             addOrModify(value.asJsonObject());
         }
+        System.out.println("Out");
+    }
+
+    public List<Building> getAllWith(double lat, double lon, double dist) {
+        double dlat = Building.getDegrees(dist / Building.R) * 1.2;
+        double lat1 = lat - dlat;
+        double lat2 = lat + dlat;
+        double dlon = dlat / cos(Building.getRadian(lon));
+        double lon1 = lon - dlon;
+        double lon2 = lon + dlon;
+        List<Building> buildings = list("geo_lat >= ?1 and geo_lat <= ?2 and geo_lon >= ?3 and geo_lon <= ?4", lat1, lat2, lon1, lon2);
+//        List<Building> buildings = listAll();
+        List<Building> result = new ArrayList<>();
+        for (var building : buildings) {
+            double dst = building.distance(lat, lon);
+            if (dst < dist)
+                result.add(building);
+        }
+        return result;
     }
 }
